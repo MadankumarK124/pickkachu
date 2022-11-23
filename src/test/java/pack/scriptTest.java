762cd6d9d4130f463e74eaef5e3514ec1faf5ba8
MADAN KUMAR K <MADAN KUMAR K@LAPTOP-IQ9B34OU>
2022-11-23 15:36:05 +0530

    commit

---
+++ src/test/java/pack/scriptTest.java
@@ -0,0 +1,18 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class scriptTest {
+	@Test
+	public void poke()
+	{
+		System.out.println("pikachu");
+	}
+	@Test
+	public void mon()
+	{
+		System.out.println("Raichu");
+	}
+	
+
+}
